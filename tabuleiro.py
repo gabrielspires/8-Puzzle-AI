0374953ea15dfa6bf64c2f210d36b5197cc1933f
Gabriel H. S. Pires <gabrielspires@live.com>
2019-04-22 10:35:16 -0300

    Tabuleiro pronto!

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,5 +1,9 @@
+#http://ozzmaker.com/add-colour-to-text-in-python/
+
+
 import Pos # Facilitar a representação das posições (tuplas)
 import numpy as np
+import pprint as pp
 
 class Tabuleiro(object):
     def __init__(self, tamanho):
@@ -12,35 +16,47 @@ class Tabuleiro(object):
     def leTabuleiro(self, pecas):
         '''Lê o tabuleiro a partir de uma string com 
         os números separados por espaço'''
-        p = pecas.split(" ")
-                
+        self.tabuleiro = np.fromstring(pecas, dtype=int, sep=" ")
+        self.tabuleiro = self.tabuleiro.reshape(self.tamanho, self.tamanho)
+        x, y = np.where(self.tabuleiro == 0)
+        self.vazio = Pos.ponto(int(x), int(y))
+        
     
     def verificaTabuleiro(self):
         '''Verifica se a disposiçao passada do 
         tabuleiro é uma solução válida.'''
-        pass
+        answer = Tabuleiro(self.tamanho)
+        return np.array_equal(answer.tabuleiro, self.tabuleiro)
     
     def trocaPeca(self, pos1, pos2):
-        '''Movimenta uma peça para o espaço vazio,
-        ou seja, troca a peça de posição com o
-        espaço vazio do tabuleiro.'''
-        pass
+        '''Troca duas pecas de posição'''
+        aux = self.tabuleiro[pos1.x][pos1.y]
+        self.tabuleiro[pos1.x][pos1.y] = self.tabuleiro[pos2.x][pos2.y]
+        self.tabuleiro[pos2.x][pos2.y] = aux
     
     def cima(self):
         '''Movimenta o espaço em branco pra cima'''
-        pass
+        if self.vazio.x > 0:
+            self.trocaPeca(self.vazio, Pos.ponto(self.vazio.x-1,self.vazio.y))
+            self.vazio = Pos.ponto(self.vazio.x-1,self.vazio.y)
     
     def baixo(self):
         '''Movimenta o espaço em branco pra baixo'''
-        pass
+        if self.vazio.x < self.tamanho-1:
+            self.trocaPeca(self.vazio, Pos.ponto(self.vazio.x+1,self.vazio.y))
+            self.vazio = Pos.ponto(self.vazio.x+1,self.vazio.y)
     
     def esquerda(self):
         '''Movimenta o espaço em branco pra esquerda'''
-        pass
+        if self.vazio.y > 0:
+            self.trocaPeca(self.vazio, Pos.ponto(self.vazio.x,self.vazio.y-1))
+            self.vazio = Pos.ponto(self.vazio.x,self.vazio.y-1)
     
     def direita(self):
         '''Movimenta o espaço em branco pra direita'''
-        pass
+        if self.vazio.y < self.tamanho-1:
+            self.trocaPeca(self.vazio, Pos.ponto(self.vazio.x,self.vazio.y+1))
+            self.vazio = Pos.ponto(self.vazio.x,self.vazio.y+1)
     
     def moverPeca(self, direcao):
         '''Executa um movimento de acordo com o
@@ -56,7 +72,20 @@ class Tabuleiro(object):
             self.moverPeca(direcao)
     
     def mostraTabuleiro(self):
-        print(self.tabuleiro)
+        for linha in self.tabuleiro:
+            for peca in linha:
+                if peca == 0: print("\033[0;33;40m",peca, end=" ")
+                else: print("\033[0;47;40m",peca, end=" ")
+            print("\033[0;47;40m")
+        print("\033[0;47;40m")
 
-a = Tabuleiro(12)
+a = Tabuleiro(3)
+a.leTabuleiro("1 2 3 4 5 6 7 0 8")
+print("Posição vazia:", a.vazio.x, a.vazio.y)
 a.mostraTabuleiro()
+# a.trocaPeca(Pos.ponto(1,1), Pos.ponto(0,1))
+# a.mostraTabuleiro()
+print(a.verificaTabuleiro())
+a.direita()
+a.mostraTabuleiro()
+print(a.verificaTabuleiro())
